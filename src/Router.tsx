8b1ca3ac17b2dcfe31138a1be50c7b686890726d
coehgns <modooboiroo@gmail.com>
2025-10-18 23:16:16 +0900

    feat :: Router.tsx

---
+++ src/Router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import { HomaPage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
+import { AppLayout } from "./layout/AppLayout";
+
+export const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/home",
+        element: <HomaPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
