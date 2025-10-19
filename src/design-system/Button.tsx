4fca567a2c822c2e619e34561abbb1293f527d39
coehgns <modooboiroo@gmail.com>
2025-10-19 22:37:33 +0900

    feat :: Button

---
+++ src/design-system/Button.tsx
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import type { ReactNode } from "react";
+
+interface IButtonProps {
+  children: ReactNode;
+  width: string;
+  height: string;
+  backgroundColor?: string;
+  borderRadius: string;
+  padding: string;
+  onClick: () => void;
+  color: string;
+  fontWeight?: string;
+}
+
+export const Button = ({
+  children,
+  width,
+  height,
+  backgroundColor = "gray",
+  onClick,
+  borderRadius,
+  padding,
+  color,
+  fontWeight,
+}: IButtonProps) => {
+  return (
+    <ButtonContainer
+      width={width}
+      height={height}
+      backgroundColor={backgroundColor}
+      onClick={onClick}
+      borderRadius={borderRadius}
+      padding={padding}
+      color={color}
+      fontWeight={fontWeight}
+    >
+      {children}
+    </ButtonContainer>
+  );
+};
+
+const ButtonContainer = styled.button<Omit<IButtonProps, "onClick" | "children">>`
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  border-radius: ${({ borderRadius }) => borderRadius};
+  display: flex;
+  padding: ${({ padding }) => padding};
+  border: none;
+  color: ${({ color }) => color};
+  font-weight: ${({ fontWeight }) => fontWeight};
+  justify-content: center;
+`;
