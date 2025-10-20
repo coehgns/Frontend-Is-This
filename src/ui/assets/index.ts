7afb48fe51f8497f043802a66a32d200b7a15c29
coehgns <modooboiroo@gmail.com>
2025-10-21 08:19:33 +0900

    feat :: index.ts에 ArrowIcons 추가

--- src/ui/assets/index.ts
+++ src/ui/assets/index.ts
@@ -1,6 +1,8 @@
 export * from "./DaedongLogo";
 export * from "./LaptopMockImg";
-export * from "./ClubsLaptopImg"
-export * from "./CreateClubLaptopImg"
-export * from "./MyPageLaptopImg"
-export * from "./NoticeLaptopImg"
+export * from "./ClubsLaptopImg";
+export * from "./CreateClubLaptopImg";
+export * from "./MyPageLaptopImg";
+export * from "./NoticeLaptopImg";
+export { default as leftArrowIcon } from "./LeftArrow.svg";
+export { default as rightArrowIcon } from "./RightArrow.svg";
