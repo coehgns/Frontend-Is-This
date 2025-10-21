18a095476c8412732045edd4e95b3db6c9e0eebc
coehgns <modooboiroo@gmail.com>
2025-10-21 17:05:18 +0900

    feat :: App.tsx에 GlobalStyle 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./Router";
+import { GlobalStyle } from "./design-system/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <RouterProvider router={Router} />
     </>
   );
